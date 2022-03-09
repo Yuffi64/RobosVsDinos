8cd33208dedfd5c6db00b204f049f20174bbc355
Yuffi64 <100430798+Yuffi64@users.noreply.github.com>
2022-03-09 12:20:18 -0600

    main updated

---
+++ main.py
@@ -0,0 +1,14 @@
+#As a developer, I want to make at least 7 commits with good, descriptive messages.
+#As a developer, I want to make a class for each of the following: Robot, Dinosaur, Fleet, Herd, Weapon, Battlefield.
+
+
+
+
+
+
+
+from battlefield import Battlefield
+
+todays_battle = Battlefield()
+
+todays_battle.run_game()
